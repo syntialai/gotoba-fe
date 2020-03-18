427ef0a0d035e07cceaec947f8aac7ba8db1f930
syntialai <53588149+syntialai@users.noreply.github.com>
2020-03-18 12:55:28 +0700

    Added const for pages and added login page

---
+++ app/src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <form>
+      <fieldset>
+        <fieldset class="form-group">
+          <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            <input id="login-username" type="email" class="form-control" name="usernameOrEmail" placeholder="Username or email">
+          </div>
+        </fieldset>
+        <fieldset class="form-group">
+          <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+            <input id="login-password" type="password" class="form-control" name="password" placeholder="Password">
+          </div>
+        </fieldset>
+
+        <router-link class="align-right" id="login-forgot-password" to="/forgot-password">Forgot password?</router-link>
+        <button class="btn btn-primary pull-xs-right" type="submit">LOG IN</button>
+
+        <span class="align-center">Don't have an account? <router-link to="/sign-up">Sign up</router-link></span> 
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
---
+++ app/src/router/pages.js
@@ -0,0 +1,15 @@
+const ABOUT = () => import('../views/About.vue');
+const LOGIN = () => import('../views/Login.vue');
+
+const Pages = {
+  ABOUT,
+  LOGIN,
+};
+
+export default Pages;
+
+// verified user
+// const ITINERARY = () => import('');
+// const PROFILE = () => import('');
+// const MY_TICKET = () => import('');
+// const CART = () => import('');
