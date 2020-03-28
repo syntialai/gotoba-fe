d99d83399b5717225189b15d9f7df2db15187cb4
syntialai <53588149+syntialai@users.noreply.github.com>
2020-03-28 20:09:54 +0700

    Added navigation component

---
+++ gotoba-fe-app/src/components/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav">
+    <b-navbar variant="faded" class="bg-transparent text-white">
+      <b-navbar-brand href="/">
+        <img src="https://placekitten.com/g/30/30"
+          class="d-inline-block align-top"
+          alt="gotoba"
+        >
+        gotoba
+      </b-navbar-brand>
+      <b-navbar-nav right>
+        <b-nav-item href="/search">
+          <b-icon icon="search"></b-icon>
+        </b-nav-item>
+        <b-nav-item href="/notification">
+          <b-icon icon="bell-fill"></b-icon>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+</script>
