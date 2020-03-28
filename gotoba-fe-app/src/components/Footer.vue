63c8c0da1f1d823c089c6b932f1a0782d242e347
syntialai <53588149+syntialai@users.noreply.github.com>
2020-03-28 15:04:24 +0700

    Added footer component

---
+++ gotoba-fe-app/src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="footer align-center">
+    <div class="bg-color-primary text-white semibold">
+      <div class="explore">
+        <h4 class="bold">EXPLORE MORE</h4>
+        <a href="">About Lake Toba</a>
+        <a href="">Destinations</a>
+        <a href="">Restaurants</a>
+        <a href="">Hotels and Homestays</a>
+        <a href="">Events</a>
+      </div>
+      <div class="contact">
+        <h4 class="bold">CONTACT US</h4>
+        <div class="contact-email">
+          <font-awesome-icon icon="envelope" />
+          <span class="contact-email-text pl-3">gotoba.service@gmail.com</span>
+        </div>
+        <div class="contact-phone">
+          <font-awesome-icon icon="phone" />
+          <span class="contact-phone-text pl-3">021-7667879</span>
+        </div>
+        <div class="contact-whatsapp">
+          <font-awesome-icon icon="whatsapp" />
+          <span class="contact-whatsapp-text pl-3">0819-9033-3364</span>
+        </div>
+      </div>
+    </div>
+    <div class="bg-color-blue-primary text-white">
+      <div class="copyright font-size-12 semibold">
+        © 2020 GoToba - FUTURE Medan
+        <br>
+        All rights reserved.
+      </div>
+      <div class="font-size-10">
+        <a href="">Terms of Use</a> |
+         <a href="">Privacy Policy</a> |
+         <a href="">Site Map</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
