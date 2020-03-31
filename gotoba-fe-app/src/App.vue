68cf54f695e954564c723c5762f6c2f0e21884a0
syntialai <53588149+syntialai@users.noreply.github.com>
2020-03-31 13:40:50 +0700

    Added terms and condition page

--- gotoba-fe-app/src/App.vue
+++ gotoba-fe-app/src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div id="app">
-    <Background />
     <router-view />
   </div>
 </template>
 
 <script>
-import Background from '@/components/Background.vue';
-
 export default {
   name: 'App',
-  components: {
-    Background,
-  },
 };
 </script>
 
--- gotoba-fe-app/src/views/Home.vue
+++ gotoba-fe-app/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Background />
     <Navigation />
     <BottomNavUser />
     <div class="container">
