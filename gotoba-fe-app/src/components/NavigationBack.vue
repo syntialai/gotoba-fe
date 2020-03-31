408b1a8ee1e1b727bcb4764ce440ef48ed03e707
syntialai <53588149+syntialai@users.noreply.github.com>
2020-03-31 13:19:08 +0700

    Added navigation back component

---
+++ gotoba-fe-app/src/components/NavigationBack.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-navbar class="bg-color-primary text-white">
+    <b-navbar-nav>
+      <b-navbar-item class="icon pr-2">
+        <p><b-icon icon="arrow-left-short" /></p>
+      </b-navbar-item>
+      <b-navbar-item class="semibold font-size-24">
+        {{ title }}
+      </b-navbar-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBack',
+  props: {
+    title: '',
+  },
+};
+</script>
