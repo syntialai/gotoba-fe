85463376a1f129a3f162735c4205ed41166f3ce9
syntialai <53588149+syntialai@users.noreply.github.com>
2020-04-03 11:15:46 +0700

    Added payment detail component for history detail

---
+++ gotoba-fe-app/src/components/PaymentDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="details font-color-black-60">
+    <div class="d-flex justify-content-between">
+      <div class="details-name">Price (estimated)</div>
+      <div class="details-value semibold">
+        {{ price }}
+      </div>
+    </div>
+    <div class="d-flex justify-content-between">
+      <div class="details-name">Promo discount</div>
+      <div class="details-value semibold">
+        {{ discount }}
+      </div>
+    </div>
+    <div class="d-flex justify-content-between bold border-top-gray-young">
+      <div class="details-name">Total Payment</div>
+      <div class="details-value semibold">
+        {{ priceTotal }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentDetail',
+  props: {
+    price: String,
+    discount: String,
+    priceTotal: String,
+  },
+  method: {
+    format(price) {
+      return new Intl.NumberFormat('id').format(parseInt(price)) + '.00';
+    },
+  },
+};
+</script>
