1904fb5c418f9e89f9d8a51f0be46eb3e74ccd20
syntialai <53588149+syntialai@users.noreply.github.com>
2020-04-07 17:28:42 +0700

    Added payment method component

---
+++ gotoba-fe-app/src/components/PaymentMethod.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="payment-method-group">
+    <b-form-group label="Stacked button style radios">
+      <b-form-radio-group
+        id="btn-radios-payment-method"
+        buttons
+        stacked
+        name="radio-btn-stacked"
+      >
+        <b-form-radio
+          class="w-100 btn btn-light"
+          v-for="method in paymentMethods"
+          :key="method.value"
+          :value="method.value"
+        >
+          <div class="payment-name">
+            {{ method.name }}
+            <b-badge>
+              <b-icon
+                icon="chevron-right"
+                class="icon icon-black-60"
+              ></b-icon>
+            </b-badge>
+          </div>
+          
+          <b-form-radio-group
+            :id="['btn-radios-' + method.value]"
+            buttons
+            stacked
+            name="radio-btn-stacked"
+            v-if="show"
+            v-model="selectedMethod"
+            :options="method.detail"
+          >
+          </b-form-radio-group>
+        </b-form-radio>
+      </b-form-radio-group>
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentMethod',
+  data() {
+    return {
+      selectedMethod: '',
+      paymentMethods: [
+        {
+          name: 'Bank Transfer',
+          value: 'bank-transfer',
+          detail: [
+            { text: 'Bank BCA', value: 'bca', },
+            { text: 'Bank BRI', value: 'bri', },
+            { text: 'Bank Mandiri', value: 'mandiri', },
+            { text: 'Others', value: 'other', },
+          ],
+        },
+        {
+          name: 'Bank Virtual Account Transfer',
+          value: 'bank-va-transfer',
+          detail: [
+            { text: 'Bank BCA', value: 'va-bca', },
+            { text: 'Bank BRI', value: 'va-bri', },
+            { text: 'Bank Mandiri', value: 'va-mandiri', },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
