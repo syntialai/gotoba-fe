79638db8b5c5392d1c0532a169b5f4e706ec90da
syntialai <53588149+syntialai@users.noreply.github.com>
2020-04-11 01:03:29 +0700

    Added autocomplete for search

---
+++ gotoba-fe-app/src/components/SearchAutocomplete.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="auto-complete overflow-hidden">
+    <vue-bootstrap-typeahead
+      :data="autocompleteList"
+      v-model="searchKeyword"
+      size="sm"
+      :serializer="s => s.text"
+      placeholder="Type an address..."
+      @hit="selectedKeyword = $event"
+    >
+  </div>
+</template>
+
+<script>
+import VueBootstrapTypeahead from 'vue-bootstrap-typeahead';
+
+export default {
+  name: 'SearchAutocomplete',
+  components: {
+    VueBootstrapTypeahead,
+  },
+  data() {
+    return {
+      autocompleteList: [],
+      searchKeyword: '',
+      selectedKeyword: null,
+    };
+  },
+};
+</script>
