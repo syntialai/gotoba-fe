f8fcb701e9d8ff3546b4a4b2ef471a588e495b25
syntialai <53588149+syntialai@users.noreply.github.com>
2020-04-16 09:28:20 +0700

    Added journey profile page

--- gotoba-fe-app/src/store/modules/journey.js
+++ gotoba-fe-app/src/store/modules/journey.js
@@ -2,24 +2,24 @@
 import * as Types from '../types';
 
 const state = {
-  restaurantData: {},
+  journeyData: {},
 };
 
 const actions = {
-  getRestaurantData({ commit }, res) {
-    commit(Types.GET_RESTAURANT_DATA, res);
+  getJourneyData({ commit }, res) {
+    commit(Types.GET_JOURNEY_DATA, res);
   },
 };
 
 const getters = {
-  restaurantData: (state) => state.restaurantData,
+  journeyData: (state) => state.journeyData,
 };
 
 const mutations = {
   // eslint-disable-next-line space-before-function-paren
-  [Types.GET_RESTAURANT_DATA](state, res) {
-		state.restaurantData = res
-	},
+  [Types.GET_JOURNEY_DATA](state, res) {
+    state.journeyData = res;
+  },
 };
 
 export default {
