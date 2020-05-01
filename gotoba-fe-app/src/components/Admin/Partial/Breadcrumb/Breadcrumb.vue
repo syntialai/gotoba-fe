86a6903301bb1fc4bdfba9039058caaa5b733e23
syntialai <53588149+syntialai@users.noreply.github.com>
2020-05-01 15:39:30 +0700

    Added breadcrumb

---
+++ gotoba-fe-app/src/components/Admin/Partial/Breadcrumb/Breadcrumb.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-breadcrumb :items="menus"></b-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: 'Breadcrumb',
+  computed: {
+    menus() {
+      const path = this.$route.path.slice(6);
+      const pathArrays = path.split('/');
+      const active = pathArrays.slice(-1);
+      pathArrays.splice(-1);
+
+      let menu = [];
+      let link = '/admin'
+      for (item in pathArrays) {
+        link += `/${item}`;
+
+        menu.push({
+          text: item,
+          to: link,
+        });
+      }
+
+      menu.push({
+        text: active,
+        active: true,
+      });
+
+      return menu;
+    },
+  },
+};
+</script>
