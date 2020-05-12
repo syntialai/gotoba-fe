8177bd5b95f9f926181ae82ffe958f68ad7d00c3
syntialai <53588149+syntialai@users.noreply.github.com>
2020-05-12 12:24:33 +0700

    Separate card from itinerary to reusable card, and added component for restaurant view

--- gotoba-fe-app/src/components/Admin/Card/ItineraryCard.vue
+++ gotoba-fe-app/src/components/Admin/Card/ItineraryCard.vue
@@ -1,62 +1,32 @@
 <template>
   <div class="itinerary-card">
-    <b-card
-      :img-src="itinerary.image"
-      :img-alt="itinerary.location"
-      img-left
-      class="p-2"
-    >
-      <b-card-body>
-        <b-card-subtitle class="bold font-color-blue-secondary">
-          {{ itinerary.location }}
-        </b-card-subtitle>
+    <data-card :data="itinerary" otherIcon="tag" />
 
-        <div class="location d-flex align-items-center">
-          <div class="location-icon icon-gradient">
-            <font-awesome-icon icon="map-marker-alt"></font-awesome-icon>
-          </div>
-          <div class="location-text pl-3">
-            {{ itinerary.location }}
-          </div>
-        </div>
-
-        <div class="price d-flex align-items-center">
-          <div class="price-icon icon-gradient">
-            <font-awesome-icon icon="tag"></font-awesome-icon>
-          </div>
-          <div class="price-text pl-3">
-            {{ itinerary.price }}
-          </div>
-        </div>
-
-        <div class="rating">
-          <rating rate="" :fontSize="14" />
-          <div class="rate-number font-size-14 semibold">
-            ({{ itinerary.rating }})
-          </div>
-        </div>
-      </b-card-body>
-
-      <template v-slot:footer>
-        <div class="bold">Description</div>
-        <div class="description">
-          {{ itinerary.description }}
-        </div>
-      </template>
-    </b-card>
+    <div class="more-itinerary-card">
+      <div class="bold">Description</div>
+      <div class="description-detail font-size-14">
+        {{ itinerary.description }}
+      </div>
+      <div class="edit-button w-100 d-flex justify-content-end">
+        <b-button
+          class="custom-btn-primary"
+          @click="this.$router.push(id)"
+        >EDIT</b-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Rating from '../../Partial/Rating.vue';
+import DataCard from './DataCard.vue';
 
 export default {
   name: 'ItineraryCard',
+  components: {
+    DataCard,
+  },
   props: {
     itinerary: Object,
-  },
-  components: {
-    Rating,
   },
 };
 </script>
---
+++ gotoba-fe-app/src/components/Admin/Card/RestaurantCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="restaurant-card">
+    <a :href="'/admin/restaurant/' + restaurant.sku">
+      <data-card :data="restaurant" otherIcon="" />
+    </a>
+  </div>
+</template>
+
+<script>
+import DataCard from './DataCard.vue';
+
+export default {
+  name: 'RestaurantCard',
+  components: {
+    DataCard,
+  },
+  props: {
+    restaurant: Object,
+  },
+};
+</script>
