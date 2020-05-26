01f0f3507a55b5442ecedf76dc3d805d10573f79
syntialai <53588149+syntialai@users.noreply.github.com>
2020-05-26 23:42:23 +0700

    Added components for merchant add itinerary/spot

---
+++ gotoba-fe-app/src/components/Merchant/Card/CardSpot.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card-spot bg-white py-2 mb-2">
+    <b-media tag="li" vertical-align="center">
+      <template v-slot:aside>
+        <b-img
+          :src="itinerary.image"
+          rounded
+          width="64"
+          :alt="itinerary.name"
+        ></b-img>
+      </template>
+      <h5 class="m-0">{{ itinerary.title }}</h5>
+      <p class="mb-0">
+        {{ itinerary.address }}
+      </p>
+      <div class="review d-flex align-items-center">
+        <rating :rate="itinerary.rate" />
+        <div class="rating-text font-size-14">
+          ({{ itinerary.rate }})
+        </div>
+      </div>
+    </b-media>
+  </div>
+</template>
+
+<script>
+import Rating from '../../Partial/Rating.vue';
+
+export default {
+  name: 'CardSpot',
+  components: {
+    Rating,
+  },
+  props: {
+    itinerary: Object,
+  },
+};
+</script>
