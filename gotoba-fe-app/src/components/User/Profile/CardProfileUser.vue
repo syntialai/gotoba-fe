5d405eec15b1e1103f8e7772654cde727f8feeb0
syntialai <53588149+syntialai@users.noreply.github.com>
2020-05-27 21:38:17 +0700

    Added axios interceptors and function to fetch itinerary

---
+++ gotoba-fe-app/src/components/User/Profile/CardProfileUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="user-profile w-100 m-4 bg-white box-shadow position-relative border-square-10">
+    <b-media vertical-align="center">
+      <template v-slot:aside>
+        <b-avatar :src="user.image"></b-avatar>
+      </template>
+      <div class="user-info">
+        <div class="user-name bold font-size-20">{{ user.nickname }}</div>
+        <div class="user-location font-color-black-60">{{ user.address }}</div>
+      </div>
+    </b-media>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardProfileUser',
+  props: {
+    user: Object,
+  },
+};
+</script>
