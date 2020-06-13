4cf32c867872f5d84a5e4aaca69d9fd69508a98b
syntialai <53588149+syntialai@users.noreply.github.com>
2020-06-13 23:48:02 +0700

    Added Add Schedule Feature components

--- gotoba-fe-app/src/components/Partial/Timeline.vue
+++ gotoba-fe-app/src/components/Partial/Timeline.vue
@@ -15,6 +15,32 @@
           </div>
         </div>
       </li>
+      <li v-if="add" @click="addSchedule">
+        <div class="d-flex justify-content-between w-100">
+          <div class="timeline-place-select font-color-black-87 semibold">
+            <b-input-group
+              size="sm"
+            >
+              <b-form-input
+                v-model="schedule.destination"
+                required
+              ></b-form-input>
+              <b-input-group-append>
+                <b-button class="custom-btn-white">
+                  <b-icon icon="chevron-down" />
+                </b-button>
+              </b-input-group-append>
+            </b-input-group>
+          </div>
+          <div class="timeline-time">
+            <b-form-timepicker
+              v-model="schedule.time"
+              required
+              locale="en"
+            ></b-form-timepicker>
+          </div>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -24,6 +50,34 @@ export default {
   name: 'Timeline',
   props: {
     timelines: Array,
+    add: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      schedule: {
+        destination: null,
+        time: null,
+      },
+      schedules: [],
+    };
+  },
+  method: {
+    addSchedule() {
+      const data = this.schedule;
+
+      if (data.destination.trim() === null || data.time === null) {
+        return;
+      }
+
+      this.schedules.push(data);
+      this.schedule = {
+        destination: null,
+        time: null,
+      };
+    },
   },
 };
 </script>
--- gotoba-fe-app/src/views/User/Itinerary/AddItinerary.vue
+++ gotoba-fe-app/src/views/User/Itinerary/AddItinerary.vue
@@ -4,6 +4,8 @@
 
     <timeline :timelines="timelines" />
 
+    <select-destination-autocomplete :showAutocomplete="showAutocomplete" />
+
     <div class="w-100 d-flex fixed-bottom" style="height: 64px;">
       <div class="w-50">
         <b-button
@@ -25,6 +27,7 @@
 
 <script>
 import ChooseDateCalendar from '../../../components/User/Itinerary/ChooseDateCalendar.vue';
+import SelectDestinationAutocomplete from '../../../components/User/Itinerary/SelectDestinationAutocomplete.vue';
 import Timeline from '../../../components/Partial/Timeline.vue';
 import api from '../../../api/api';
 
@@ -32,11 +35,13 @@ export default {
   name: 'AddItinerary',
   components: {
     ChooseDateCalendar,
+    SelectDestinationAutocomplete,
     Timeline,
   },
   data() {
     return {
       timelines: [],
+      showAutocomplete: false,
     };
   },
   method: {
